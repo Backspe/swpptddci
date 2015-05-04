f832980ffb1fd915a51be6738402c0e18a8ece53
Backspe <bschoe@gmail.com>
2015-05-04 20:15:49 +0900

    implement add, remove

--- impl/Multiset.rb
+++ impl/Multiset.rb
@@ -6,4 +6,13 @@ class Multiset
   def contains(n)
     return @a.include?(n)
   end
+
+  def add(n)
+    @a.push(n)
+  end
+
+  def remove(n)
+    i = nil
+    @a.delete_at(i) if i = @a.index(n)
+  end
 end
